de719a8acefd19ac32ab203a012f7ce602746c56
Andrew Edwards <andrewangeta@gmail.com>
2018-01-28 12:33:23 -0500

    Clean up enum

--- Sources/Telesign/API/TelesignTypes.swift
+++ Sources/Telesign/API/TelesignTypes.swift
@@ -49,35 +49,6 @@ public enum VoiceLanguage: String, Codable
     case spanishSpain = "f-es-ES"
     case spanishCatalan = "f-ca-ES"
     case swedish = "f-sv-SE"
-    
-    enum CodingKeys: String, CodingKey
-    {
-        case chineseHongKong = "f-zh-HK"
-        case chineseMandarin = "f-zh-CN"
-        case chineseTaiwan = "f-zh-TW"
-        case danish = "f-da-DK"
-        case dutch = "f-nl-NL"
-        case englishAustralian = "f-en-AU"
-        case englishUK = "f-en-GB"
-        case englishUS = "f-en-US"
-        case englishCanadian = "f-en-CA"
-        case englishIndia = "f-en-IN"
-        case finnish = "f-fi-FI"
-        case french = "f-fr-FR"
-        case frenchCanadian = "f-fr-CA"
-        case german = "f-de-DE"
-        case italian = "f-it-IT"
-        case japanese = "f-ja-JP"
-        case korean = "f-ko-KR"
-        case norweigan = "f-nb-NO"
-        case portugueseBrazil = "f-pt-BR"
-        case portugueseEurope = "f-pt-PT"
-        case russian = "f-ru-RU"
-        case spanish = "f-es-MX"
-        case spanishSpain = "f-es-ES"
-        case spanishCatalan = "f-ca-ES"
-        case swedish = "f-sv-SE"
-    }
 }
 
 public enum VoiceLanguageOrUserInputResponse: Codable
